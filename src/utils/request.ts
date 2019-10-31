95a38f8971dcf2f2a48c5bdd14d49cd5e99b0a5a
scater <741170471@qq.com>
2019-10-31 20:21:09 +0800

    :sparkles: axios拦截简单配置

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,37 @@
 import axios from 'axios';
+// import { Loading } from 'element-ui';
+
+const serve = axios.create({
+    baseURL: 'https://some-domain.com/api/',
+    timeout: 1000,
+    headers: { 'X-Custom-Header': 'foobar' }
+})
+
+// let loadingInstance = Loading.service({});
+
+// 请求拦截器
+serve.interceptors.request.use(config => {
+    // loadingInstance.open()
+    const token = localStorage.getItem('userToken');
+    if (token) { // 判断是否存在token，如果存在的话，则每个http header都加上token
+      config.headers.accessToken = token;
+    }
+    return config;
+  }, 
+  error => {
+    return Promise.reject(error);
+  })
+
+// 响应拦截器
+serve.interceptors.response.use(response => {
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  })
 
 export default {
-    install (Vue: any) {
-        Vue.prototype.$axios = axios;
+    install(Vue: any) {
+        Vue.prototype.$axios = serve;
     }
 }
